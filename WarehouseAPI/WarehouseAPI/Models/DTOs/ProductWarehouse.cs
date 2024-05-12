2f13009d11dfc6f2460e1f46771f9b088bf796e3
s27799 <s27799@pjwstk.edu.pl>
2024-05-12 22:24:14 +0200

    added: validation

--- WarehouseAPI/WarehouseAPI/Models/DTOs/ProductWarehouse.cs
+++ WarehouseAPI/WarehouseAPI/Models/DTOs/ProductWarehouse.cs
@@ -1,9 +1,15 @@
-﻿namespace WarehouseButBetter.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseButBetter.Models;
 
 public class ProductWarehouse
 {
+    [Required]
     public int IdProduct { get; set; }
+    [Required]
     public int IdWarehouse { get; set; }
+    [Required]
     public int Amount { get; set; }
+    [Required]
     public DateTime CreatedAt { get; set; }
 }
